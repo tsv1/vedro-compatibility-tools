a50eff78a7f44bda31fc42888533bc77a37215a8
Nikita Tsvetkov <nikitanovosibirsk@yandex.com>
2022-04-17 21:56:26 +0300

    add config

--- vedro_compatibility_tools/__init__.py
+++ vedro_compatibility_tools/__init__.py
@@ -1,23 +1,15 @@
-from typing import Optional
+from typing import Optional, Type
 
+import valeera
 from blahblah import Substitutor
 from district42 import SchemaType
-from valeera import AbstractFormatter, AbstractValidator, Formatter, Validator
-from vedro.core import Dispatcher, Plugin
+from valeera import AbstractFormatter, Formatter
+
+from vedro.core import Dispatcher, Plugin, PluginConfig
 from vedro.events import ExceptionRaisedEvent
 
 
-def register(validator: Optional[AbstractValidator] = None,
-             substitutor: Optional[Substitutor] = None) -> None:
-    if validator is None:
-        validator = Validator(Formatter())
-    if substitutor is None:
-        substitutor = Substitutor()
-    SchemaType.__eq__ = lambda a, b: validator.validate(b, a).passes()
-    SchemaType.__mod__ = lambda self, val: self.accept(substitutor, val)
-
-
-class SchemaValidator(Validator):
+class Validator(valeera.Validator):
     def __init__(self, formatter: Optional[AbstractFormatter] = None) -> None:
         super().__init__(formatter if formatter else Formatter())
 
@@ -26,13 +18,18 @@ class SchemaValidator(Validator):
 
 
 class SchemaValidationPlugin(Plugin):
-    def __init__(self, validator: SchemaValidator) -> None:
-        self._validator = validator
+    def __init__(self, config: Type["SchemaValidator"]) -> None:
+        super().__init__(config)
+        self._validator = Validator()
+        self._substitutor = Substitutor()
 
     def subscribe(self, dispatcher: Dispatcher) -> None:
-        dispatcher.listen(ExceptionRaisedEvent, self.on_excpetion_raised)
+        SchemaType.__eq__ = lambda a, b: self._validator.validate(b, a).passes()
+        SchemaType.__mod__ = lambda s, v: s.accept(self._substitutor, v)
 
-    def on_excpetion_raised(self, event: ExceptionRaisedEvent) -> None:
+        dispatcher.listen(ExceptionRaisedEvent, self.on_exception_raised)
+
+    def on_exception_raised(self, event: ExceptionRaisedEvent) -> None:
         if event.exc_info.type is not AssertionError:
             return
         errors = self._validator.errors()
@@ -45,5 +42,9 @@ class SchemaValidationPlugin(Plugin):
         event.exc_info.type = type(exception)
 
 
-__all__ = ("SchemaValidator", "SchemaValidationPlugin", "register",)
-__version__ = "0.1.2"
+class SchemaValidator(PluginConfig):
+    plugin = SchemaValidationPlugin
+
+
+__all__ = ("SchemaValidator", "SchemaValidationPlugin",)
+__version__ = "0.2.0"
