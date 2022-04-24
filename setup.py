37b1149d7473d0a7bcfeae98402293331f7a6759
Nikita <nikitanovosibirsk@yandex.com>
2022-04-24 16:10:25 +0300

    add config (#1)

--- setup.py
+++ setup.py
@@ -8,13 +8,13 @@ def find_required():
 
 setup(
     name="vedro_compatibility_tools",
-    version="0.1.2",
+    version="1.0.0",
     description="",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     author="Nikita Tsvetkov",
     author_email="nikitanovosibirsk@yandex.com",
-    python_requires=">=3.7",
+    python_requires=">=3.8",
     url="https://github.com/nikitanovosibirsk/vedro-compatibility-tools",
     license="Apache-2.0",
     packages=find_packages(),
@@ -22,7 +22,6 @@ setup(
     tests_require=[],
     classifiers=[
         "License :: OSI Approved :: Apache Software License",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
